73c245599444ca67cd3fd0d90ab7e6067f7e59ca
Luiz Felipe de Oliveira <luiz.pepe@hotmail.com>
2022-03-10 10:55:30 -0300

    Desafio Final

---
+++ ComprasNoSupermercado/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class ComprasNoSupermercado
+{
+    public static void Main(string[] args)
+    {
+        int totalDeCasosDeTeste = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < totalDeCasosDeTeste; i++)
+        {
+            List<string> listDeCompras = new List<string>(Console.ReadLine().Split(' '));
+            List<string> listaSemDuplicados = listDeCompras.Distinct().ToList();
+            listaSemDuplicados.Sort();
+
+            foreach (var itens in listaSemDuplicados)
+            {
+                Console.Write($"{itens} ");
+            }
+
+            Console.WriteLine(" ");
+        }
+    }
+}
